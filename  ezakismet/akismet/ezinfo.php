a9ba9ed4fff8b243a3886d08853474879b947da4
project@contactivity.com <project@contactivity.com@76a65ed2-e31b-11de-b950-c1e2c078a2c5>
2009-12-07 10:37:15 +0000

    Initial import

---
+++  ezakismet/akismet/ezinfo.php
@@ -0,0 +1,26 @@
+<?php
+
+class akismetInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Akismet eZ publish extension",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2009 Contactivity BV",
+            'Author' => "Contactivity bv, based on the Akismet extension by Kristof Coomans for eZ publish 3.x ",
+            'License' => "GNU General Public License v2.0",
+            'Includes the following third-party software' =>
+                array(
+                    'Name' => "Akismet PHP5 class",
+                    'Version' => "0.4",
+                    'Patches' => "Make submitSpam and submitHam methods return the response",
+                    'Website' => 'http://www.miphp.net/blog/view/php4_akismet_class/',
+                    'License' => "http://www.opensource.org/licenses/mit-license.php MIT License",
+                    'Author' => "Bret Kuhns"
+                )
+        );
+    }
+}
+
+?>
---
+++  ezakismet/akismet/modules/akismet/module.php
@@ -0,0 +1,21 @@
+<?php
+
+$Module = array( 'name' => 'Akismet', 'variable_params' => false, 'ui_component_match' => 'module' );
+
+$ViewList = array();
+$ViewList['reportspam'] = array(
+    'script' => 'reportspam.php',
+    'functions' => array( 'submit' ),
+    'unordered_params' => array( 'offset' => 'Offset' ),
+    'single_post_actions' => array(
+        'SpamSubmitButton' => 'Submit',
+        'SpamRemoveButton' => 'Remove' ),
+    'post_action_parameters' => array(
+        'Submit' => array( 'ObjectIDList' => 'ObjectIDList' ),
+        'Remove' => array( 'ObjectIDList' => 'ObjectIDList' ) )
+);
+
+$FunctionList = array();
+$FunctionList['submit'] = array();
+
+?>
